def9c91d677e240e380195f0c0b5d272bc12483d
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-08-24 15:08:49 +0100

    Updated callback route

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -47,8 +47,8 @@ module.exports = function (app, passport, io) {
     console.log(`${process.env.API_URL}/auth/twitter/callback/${req.params.instance}`)
     passport.authenticate('twitter', {
       callbackURL: `${process.env.API_URL}/auth/twitter/callback/${req.params.instance}`,
-      successRedirect: process.env.DEV_MODE ? `http://ca.local:8080/profile` : `https://${req.params.instance}.connectedacademy.io/profile`,
-      failureRedirect: process.env.DEV_MODE ? `http://ca.local:8080/auth` : `https://${req.params.instance}.connectedacademy.io/auth`
+      successRedirect: (process.env.ENVIRONMENT == 'production') ? `https://${req.params.instance}.connectedacademy.io/profile` : 'http://ca.local:8080/profile',
+      failureRedirect: (process.env.ENVIRONMENT == 'production') ? `https://${req.params.instance}.connectedacademy.io/auth` : 'http://ca.local:8080/auth'
     })(req, res, next)
   })
 }
